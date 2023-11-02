0eea1fe4762cd62ab94fa4008540080e3812f325
Asim <asimvyl@gmail.com>
2023-11-02 21:22:56 +0530

    user login finished

--- vender/views.py
+++ vender/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
+
+from .forms import CustomUserCreationForm 
+from .models import Vender
 
 def join_vender(request):
-    return render(request,"join_vender.html")
-
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST) 
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            vender = Vender.objects.create(name=user.username, created_by=user)
+            return redirect('front_page')
+    else:
+        form = CustomUserCreationForm()  
+    return render(request, "join_vender.html", {"form": form})
